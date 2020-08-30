bb60f66e39cda8dbb2eaed35c78606b82d6d5def
gurkaran97 <70472921+gurkaran97@users.noreply.github.com>
2020-08-30 18:31:58 +0530

    Add files via upload

---
+++ run.py
@@ -0,0 +1,16 @@
+import hashlib
+hashed_word = input('Enter the hashed word: ')
+pass_file = input('Enter the password file: ')
+try:
+    passfile = open(pass_file, "r")
+except:
+    print("No file found, please check again")
+for word in passfile:
+    enc_word = word.encode()
+    passfile_hashes = hashlib.md5(enc_word).hexdigest()
+    if passfile_hashes == hashed_word:
+        print('Password found. Password is ' + passfile_hashes)
+        break
+    else:
+        print("No password found")
+        break
